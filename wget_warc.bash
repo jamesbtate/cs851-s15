22999d94bc1a17bd74bff5249e2c130561ebd73a
James Tate II <jtate@cs.odu.edu>
2015-03-04 19:46:50 -0500

    moved report stuff to separate directories for hw1 and hw2 slightly modified wget_warc.bash to download supporting files

--- wget_warc.bash
+++ wget_warc.bash
@@ -15,5 +15,5 @@ do
     echo "$count"
     id=$(echo "$line" | cut -d' ' -f 1)
     uri=$(echo "$line" | cut -d' ' -f 2)
-    wget --warc-file="$2/$id" -p -l 1 "$uri" > "$2/${id}.wget.output" 2>&1
+    wget --warc-file="$2/$id" -p -l 1 -H -e robots=off -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' -P "garbage/" "$uri" > "$2/${id}.wget.output" 2>&1
 done < $1
